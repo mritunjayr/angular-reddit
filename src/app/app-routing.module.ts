d0516838d9ee0c7a26ecc607dbdc1c0273d25e1e
mritunjay kumar <mritunjaykuar694@gmail.com>
2023-08-15 20:30:24 +0530

    updated to 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [{ path: 'sign-up', component: SignupComponent },
 { path: 'login', component: LoginComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
